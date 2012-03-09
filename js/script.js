a0ca6c40e1c1bf9ea43bfe2677ca1629d0faac70
ArmNo <arm@snapplabs.com>
2012-03-09 11:53:12 +0700

    shows random flickr photos on homepage

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
-/* Author:
+/* Author: Absolutely ArmNo!
 
 */
+
+$(document).ready(function() {
+	var page = Math.random() * (1000 - 1) + 1;
+	$.getJSON('http://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=33be3c47d126a1b87570e8e0d905fc13&user_id=64097785%40N07&per_page=1&page=1&format=json&nojsoncallback=1&extras=url_m&page=' + page,
+		null,
+		function(res) {
+			if (res.stat === 'ok') {
+
+				var src = res.photos.photo[0].url_m,
+					title = res.photos.photo[0].title;
+
+				$('#random-photo').attr({
+					'src' : src,
+					'alt' : title,
+					'title' : title
+				}).fadeIn();
+
+				$('#photo-title').hide().text(title).fadeIn();
+			}
+		});
+});
 
 
 
