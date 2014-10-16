e22122427ce345e3dcc360d2aa2a1c11d0ef2c94
Armno <monkeyarmno@gmail.com>
2014-10-16 14:52:06 +0700

    use https for flickr api

--- js/script.js
+++ js/script.js
@@ -1,14 +1,10 @@
-/* Author: Absolutely ArmNo!
-
-*/
-
 $(document).ready(function() {
 
 	$('#website-name, footer').hide();
 
 	// randomly shows armno's flickr photos
 	var page = Math.random() * (1000 - 1) + 1;
-	$.getJSON('http://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=33be3c47d126a1b87570e8e0d905fc13&user_id=64097785%40N07&per_page=1&page=1&format=json&nojsoncallback=1&extras=url_m&page=' + page,
+	$.getJSON('https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=33be3c47d126a1b87570e8e0d905fc13&user_id=64097785%40N07&per_page=1&page=1&format=json&nojsoncallback=1&extras=url_m&page=' + page,
 		null,
 		function(res) {
 			if (res.stat === 'ok') {
